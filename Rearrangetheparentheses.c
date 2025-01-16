f5cd409b9d353e081255c10f5531da354a45bbda
Syed Shams Nasir <113261850+VIGILANT2@users.noreply.github.com>
2025-01-16 20:48:15 +0600

    Update Rearrangetheparentheses.c

--- Rearrangetheparentheses.c
+++ Rearrangetheparentheses.c
@@ -2,7 +2,7 @@
 
 int main() {
     int a = 1, b = 2, c = 3, d = 4, e = 2;
-    float res1 = (a + b) * c - d / (float)e; // Standard precedence without changing order
-    printf("%.2f\n", res1);  // Output: 5.00
+    float res1 = (a + b) * c - d / (float)e;
+    printf("%.2f\n", res1);
     return 0;
 }
